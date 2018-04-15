4884be1a4604d990a79bb2e8c39773cdec761e79
nakabonne <rodriguez.nak@gmail.com>
2018-04-15 16:19:58 +0900

    create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if type "brew"; then
+  echo "$(tput setaf 2)Already installed Homebrew ✔︎$(tput sgr0)"
+else
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if type "brew"; then
+  brew install ripgrep
+fi
