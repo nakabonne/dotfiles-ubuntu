516fc15fc910269da54cb64541463606dcab49ce
nakabonne <rodriguez.nak@gmail.com>
2018-04-17 11:30:30 +0900

    add go

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+# brew
 if type "brew"; then
   echo "$(tput setaf 2)Already installed Homebrew ✔︎$(tput sgr0)"
 else
   echo "Installing Homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
 if type "brew"; then
   brew install ripgrep
+  brew install go
+fi
+
+
+# go
+if type "go"; then
+  echo "$(tput setaf 2)Already installed Go ✔︎$(tput sgr0)"
+else
+  brew install go
+fi
+if type "go"; then
+  go get github.com/motemen/ghq
 fi
