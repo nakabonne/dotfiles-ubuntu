a6b4cb76d4585efb05fab82123fdbb493604e0eb
nakabonne <rodriguez.nak@gmail.com>
2018-04-17 17:43:37 +0900

    add neovim

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ if type "brew"; then
   brew install ripgrep
   brew install go
   brew install fzf
+  brew install neovim
+  brew install python3
 fi
 
 
@@ -33,4 +35,15 @@ else
 fi
 if type "ghq"; then
   ghq get https://github.com/b4b4r07/enhancd
+fi
+
+
+# pip3 
+if type "pip3"; then
+  echo "$(tput setaf 2)Already installed pip3 ✔︎$(tput sgr0)"
+else
+  brew install python3
+fi
+if type "pip3"; then
+  pip3 install neovim
 fi
